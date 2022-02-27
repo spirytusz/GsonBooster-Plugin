b1249b589826b461f61d6e6e44129888821f7aec
zkw012300 <zhangwel261717@gmail.com>
2022-02-27 18:57:57 +0800

    移动目录

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,10 +2,10 @@ rootProject.name = "GsonBooster-Plugin"
 include("plugin-main")
 
 include(":base:check")
-project(":base:check").projectDir = File("../../AndroidStudioProjects/GsonBooster/base/check")
+project(":base:check").projectDir = File("../AndroidStudioProjects/GsonBooster/base/check")
 
 include(":base:gen")
-project(":base:gen").projectDir = File("../../AndroidStudioProjects/GsonBooster/base/gen")
+project(":base:gen").projectDir = File("../AndroidStudioProjects/GsonBooster/base/gen")
 
 include(":base:processor-base")
-project(":base:processor-base").projectDir = File("../../AndroidStudioProjects/GsonBooster/base/processor-base")
+project(":base:processor-base").projectDir = File("../AndroidStudioProjects/GsonBooster/base/processor-base")
